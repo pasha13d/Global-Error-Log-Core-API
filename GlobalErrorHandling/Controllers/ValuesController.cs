c980f5e48d0679f36ece55b9ebc3b93963218bf1
pasha13d <mahabub13d1995@gmail.com>
2020-12-23 20:46:41 +0600

    core api global error handling using log

---
+++ GlobalErrorHandling/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using LoggerService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalErrorHandling.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private ILoggerManager _logger;
+        public ValuesController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
